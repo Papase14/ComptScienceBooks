4c2e304ea35ae4df703a4e294b21215f1657ea27
Tshupane Morake <tshupane@outlook.com>
2024-05-29 23:41:43 +0200

    update

--- obj/Debug/net8.0/ComptScienceBooks.AssemblyInfo.cs
+++ obj/Debug/net8.0/ComptScienceBooks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComptScienceBooks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65c9094db01cdad987e50fc655bb38de9c89ec85")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComptScienceBooks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComptScienceBooks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
